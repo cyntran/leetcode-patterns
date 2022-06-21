3fc892ab49662212e4a9720ff5238f28b5d57b76
cyntran <cynthiamytran@gmail.com>
2022-06-21 13:32:13 -0500

    work in progress for #76

--- 1-sliding_window.js
+++ 1-sliding_window.js
@@ -78,7 +78,97 @@ var numOfSubarrays_2 = function (arr, k, threshold) {
   return counter;
 }
 
-let numOfSets = numOfSubarrays_2(arr, k, threshold);
+// let numOfSets = numOfSubarrays_2(arr, k, threshold);
 
 // expected output 3
-console.log(numOfSets)
+// console.log(numOfSets)
+
+// Leetcode problem #76 (Hard)
+// Minimum Window Substring
+
+var minWindow = function (s, t) {
+  // // Build a hashmap of t chars and number of ocurrences 
+  let targetMap = initTargetMap(t);
+
+  let index = 0;
+  let numberOfTFound = 0;
+  let shortestSubstr = "";
+  let searchIndex = -1;
+  for (let i = 0; i < s.length; i++) {
+    if (targetMap[s[i]] && targetMap[s[i]] > 0) {
+      searchIndex = i; // 0
+      index = i; // 0
+      break;
+    }
+  }
+
+  console.log('searchIndex:', searchIndex)
+
+  if (searchIndex < 0) {
+    return shortestSubstr;
+  }
+
+  let itrS = s.slice(searchIndex); // "bba"
+  console.log(itrS)
+  let nextSearchIndex = 0;
+  let nextSearchSet = false;
+
+  // s:"bb/a"
+  // t: "ab"
+  // index: 3
+  // searchIndex: 2
+  // numberOfTFound 1
+  // nextSearchIndex: 0
+  // itrS: "ba"
+  // shortestSubstr = ""
+  while (itrS.length) {
+    // The character exists, but we've already found the needed # of occurences
+    // and it's the second occurence we care about...
+    if (targetMap[s[index]] === 0 && numberOfTFound === 1) {
+      // We still want to keep track of it
+      console.log(s[index], index) // 1
+      nextSearchIndex = index;
+      nextSearchSet = true;
+    }
+
+    if (targetMap[s[index]] > 0) { // { a:1, b:1 }
+      numberOfTFound++;
+      targetMap[s[index]]--;
+
+      if (numberOfTFound === 2 && !nextSearchSet) {
+        nextSearchIndex = index; // 2
+      }
+
+      if (numberOfTFound === t.length) {
+        let foundSubstr = s.substring(searchIndex, index + 1); // (incl,excl) //foundSubstr = "bba"
+        searchIndex = nextSearchIndex; // 2
+
+        if (!shortestSubstr.length) {
+          shortestSubstr = foundSubstr;
+        }
+
+        shortestSubstr = (shortestSubstr.length > foundSubstr.length) ? foundSubstr : shortestSubstr; // "bba"
+        itrS = s.slice(searchIndex - 1);  // ba
+        numberOfTFound = 0;
+        targetMap = initTargetMap(t);
+        index = searchIndex - 1;
+      }
+    }
+
+    index++;
+    itrS = itrS.slice(1);
+  }
+
+  return shortestSubstr;
+}
+
+function initTargetMap(t) {
+  let targetMap = {};
+  for (let i = 0; i < t.length; i++) {
+    targetMap[t[i]] = targetMap[t[i]] + 1 || 1;
+  }
+  return targetMap;
+}
+
+let shortestSubstrTest = minWindow("bba", "ab");
+console.log("shortest:", shortestSubstrTest)
