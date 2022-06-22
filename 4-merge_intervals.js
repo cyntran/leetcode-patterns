2779bf1daa7a51478e7250ef60e686b92354a761
cyntran <cynthiamytran@gmail.com>
2022-06-21 21:51:36 -0500

    4

---
+++ 4-merge_intervals.js
@@ -0,0 +1,13 @@
+// Merge intervals
+// When you need to find interlapping intervals.
+// There are six different ways they interlap or relate to each other.
+
+// How to identify a merge interval pattern:
+// - If you're asked to produce a list with mutually exclusive intervals
+// - If you hear the term "overlapping intervals"
+
+// Leetcode merge interval questions
+// - Merge intervals #56 (medium)
+// - Maximum CPU load #621 (hard)
+
+
